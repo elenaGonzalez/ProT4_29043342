feb66b1a0ea675d3d3b788909d7ba6638d28a3b5
elenaGonzalez <elebeatrizgonzakez@gmail.com>
2025-07-10 20:57:57 -0300

    Agrega toLowerCase para la insertar y editar libros

--- src/libroController.js
+++ src/libroController.js
@@ -1,5 +1,5 @@
 import { pool } from './database.js';
-import {validateLibro, validateId, validateType, validateISBN} from './validation.js';
+import { validateLibro, validateId, validateType, validateISBN } from './validation.js';
 
 class LibroController {
     async getAll(req, res) {
@@ -15,10 +15,10 @@ class LibroController {
 
     async getOne(req, res, next) {
         const id = Number(req.params.id);
-         validateType(id); 
+        validateType(id, "id");
         try {
             const [result] = await pool.query('SELECT * from libros WHERE id= ?', [id]);
-            validateId(id, result);   
+            validateId(id, result);
             res.json(result);
         } catch (error) {
             next(error);
@@ -28,8 +28,13 @@ class LibroController {
     async add(req, res, next) {
         const libro = req.body;
         validateLibro(libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN);
+        let nombre = libro.nombre.toLowerCase();
+        let autor = libro.autor.toLowerCase();
+        let categoria = libro.categoria.toLowerCase();
+        let año_publicacion = libro.año_publicacion.toLowerCase();
+        let ISBN = libro.ISBN.toUpperCase();
         try {
-            const [result] = await pool.query(`INSERT into libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES(? , ?, ? , ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+            const [result] = await pool.query(`INSERT into libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES(? , ?, ? , ?, ?)`, [nombre, autor, categoria, año_publicacion, ISBN]);
             res.json({ "Id insertado ": result.insertId });
         } catch (error) {
             next(error);
@@ -38,22 +43,27 @@ class LibroController {
 
     async delete(req, res, next) {
         const ISBN = Number(req.body.ISBN);
-         validateType(ISBN);   
+        validateType(ISBN, "ISBN");
         try {
             const [result] = await pool.query(`DELETE FROM libros WHERE ISBN=(?)`, [ISBN]);
-             validateISBN(ISBN, result.affectedRows);   
+            validateISBN(ISBN, result.affectedRows);
             res.json({ "Registro eliminado": result.affectedRows });
         } catch (error) {
-             next(error);
+            next(error);
         }
     }
 
     async update(req, res, next) {
         const libro = req.body;
-         validateType(libro.id);   
+        validateType(libro.id, "Id");
+        let nombre = libro.nombre.toLowerCase();
+        let autor = libro.autor.toLowerCase();
+        let categoria = libro.categoria.toLowerCase();
+        let año_publicacion = libro.año_publicacion.toLowerCase();
+        let ISBN = libro.ISBN.toUpperCase();
         try {
-            const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?) , ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
-            validateId(libro.id, result.changedRows); 
+            const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?) , ISBN=(?) WHERE id=(?)`, [nombre, autor, categoria, año_publicacion, ISBN, id]);
+            validateId(libro.id, result.changedRows);
             res.json({ "Registro actualizado ": result.changedRows });
         } catch (error) {
             next(error);
